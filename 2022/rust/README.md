ab6acbfc27cec8cc1b3a3ceab6cd979d540ecab3
Rafael Zuquim <rzuquim@inoa.com.br>
2023-11-16 19:21:28 -0300

    2022_rust_day01: [refactor] moving cli and parsing utils into separate file

--- 2022/rust/README.md
+++ 2022/rust/README.md
@@ -5,5 +5,5 @@ Using AOC to learn and play around with a new language.
 > NOTE:
 > Since I'm learning here, and this is my first attempt to write rust
 > the code won't be idiomatic nor performant. After each attempt I will
-> try to improve the solutions looking for other more repos on github.
+> try to improve the solutions looking for other repos on github.
 
--- 2022/rust/day_01/src/main.rs
+++ 2022/rust/day_01/src/main.rs
@@ -1,17 +1,14 @@
-use std::io::BufRead;
-use std::{env, fs::File, io, path::Path};
+
+mod utils;
 
 fn main() {
-    let (input_file, verbose) = parse_args();
-
-    let path = Path::new(&input_file);
-    let file = File::open(path).expect(format!("Could not open file {}", &input_file).as_str());
-    let reader = io::BufReader::new(file);
+    let (input_file, verbose) = utils::parse_args();
 
     let mut max_calories = [0, 0, 0];
     let mut curr_acc = 0;
     let mut elve_index = 0;
-    for line in reader.lines() {
+
+    for line in utils::yield_lines(&input_file) {
         if let Some(calories) = parse_calories(line) {
             curr_acc = curr_acc + calories;
         } else {
@@ -77,29 +74,3 @@ fn is_greater_than_previous_max(curr_acc: &i32, max_calories: &[i32]) -> Option<
     };
 }
 
-fn parse_args() -> (String, bool) {
-    let args = env::args().collect::<Vec<String>>();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <input_file> <verbose flag?>", args[0]);
-        std::process::exit(1);
-    }
-
-    let input_file = &args[1];
-    if args.len() < 3 {
-        return (input_file.clone(), false);
-    }
-
-    let verbose = match args[2].to_lowercase().as_str() {
-        "true" | "-v" => true,
-        "false" => false,
-        _ => {
-            eprintln!(
-                "Unexpected verbose flag (use true, false or -v): '{}'",
-                args[2]
-            );
-            std::process::exit(-1);
-        }
-    };
-
-    return (input_file.clone(), verbose);
-}
